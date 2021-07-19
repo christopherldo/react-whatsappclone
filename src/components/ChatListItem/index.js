9fe90c80953c5851b0be54c232899dd1ecabfc57
Christopher de Oliveira <christopherforwork@gmail.com>
2021-07-19 16:56:15 -0400

    :construction: Sidebar ChatList 1/2

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './style.css';
 
-const ChatListItem = () => <div>...</div>;
+const ChatListItem = () => (
+  <div className="chatListItem">
+    <img
+      className="chatListItem--avatar"
+      src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+      alt="Avatar"
+    />
+
+    <div className="chatListItem--lines">
+      <div className="chatListItem--line">
+        <div className="chatListItem--name">Christopher de Oliveira</div>
+
+        <div className="chatListItem--date">19:00</div>
+      </div>
+
+      <div className="chatListItem--line">
+        <div className="chatListItem--lastMsg">
+          <p>Opa, bão?</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default ChatListItem;
