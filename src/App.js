c372ba3f33e0dae82a37ce9297859b665c41a077
Christopher de Oliveira <christopherforwork@gmail.com>
2021-07-19 16:01:30 -0400

    :construction: Sidebar and ContentArea Interface Division

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="app-window">
+    <div className="sidebar">
+      ...
     </div>
-  );
-}
+
+    <div className="contentarea">
+      ...
+    </div>
+  </div>
+);
 
 export default App;
