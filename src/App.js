6570c3f4b83f535097d21ec08df7ce2173f5d0fc
Christopher de Oliveira <christopherforwork@gmail.com>
2021-07-19 16:19:26 -0400

    :construction: Sidebar Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './App.css';
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 const App = () => (
   <div className="app-window">
     <div className="sidebar">
-      ...
+      <header>
+        <img className="header--avatar" src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" alt="Avatar" />
+
+        <div className="header--buttons">
+          <div className="header--btn">
+            <DonutLargeIcon />
+          </div>
+
+          <div className="header--btn">
+            <ChatIcon />
+          </div>
+
+          <div className="header--btn">
+            <MoreVertIcon />
+          </div>
+        </div>
+      </header>
+
+      <div className="search">
+        ...
+      </div>
+
+      <div className="chatlist">
+        ...
+      </div>
     </div>
 
     <div className="contentarea">
