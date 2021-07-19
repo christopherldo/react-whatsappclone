9be77301f8b0b79f5c5a97f797c058e64cfbf6c8
Christopher de Oliveira <christopherforwork@gmail.com>
2021-07-19 16:38:10 -0400

    :construction: Sidebar Search

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import ChatListItem from './components/ChatListItem';
 
-const App = () => (
-  <div className="app-window">
-    <div className="sidebar">
-      <header>
-        <img className="header--avatar" src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" alt="Avatar" />
+const App = () => {
+  const [chatList, setChatList] = useState([]);
 
-        <div className="header--buttons">
-          <div className="header--btn">
-            <DonutLargeIcon />
+  useEffect(() => {
+    setChatList([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]);
+  }, []);
+
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img
+            className="header--avatar"
+            src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+            alt="Avatar"
+          />
+
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon />
+            </div>
+
+            <div className="header--btn">
+              <ChatIcon />
+            </div>
+
+            <div className="header--btn">
+              <MoreVertIcon />
+            </div>
           </div>
+        </header>
 
-          <div className="header--btn">
-            <ChatIcon />
-          </div>
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon />
 
-          <div className="header--btn">
-            <MoreVertIcon />
+            <input
+              type="search"
+              placeholder="Procurar ou começar uma nova conversa"
+            />
           </div>
         </div>
-      </header>
 
-      <div className="search">
-        ...
+        <div className="chatlist">
+          {chatList.map((item) => (
+            <ChatListItem key={item.id} />
+          ))}
+        </div>
       </div>
 
-      <div className="chatlist">
-        ...
-      </div>
+      <div className="contentarea">...</div>
     </div>
-
-    <div className="contentarea">
-      ...
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css';
+
+const ChatListItem = () => <div>...</div>;
+
+export default ChatListItem;
