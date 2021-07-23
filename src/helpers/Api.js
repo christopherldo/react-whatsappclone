d032502ebb7c296a6579c856e5fb1905612272b9
Christopher de Oliveira <christopherforwork@gmail.com>
2021-07-23 12:23:38 -0400

    :construction: Configuring firebase on project

---
+++ src/helpers/Api.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/firebase-firestore';
+
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyANICJaOvCyG9XRaCh0rtK0UIuAhrm8PF4',
+  authDomain: 'react-whatsappclone.firebaseapp.com',
+  projectId: 'react-whatsappclone',
+  storageBucket: 'react-whatsappclone.appspot.com',
+  messagingSenderId: '784776353266',
+  appId: '1:784776353266:web:8a291dbdf5708140b2c76b',
+};
+
+export default firebaseConfig;
